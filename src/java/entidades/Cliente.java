bba45fa06f8e9f8e593185e03f60542489488daa
bayron <bayrontakeoch21@gmail.com>
2023-12-20 11:33:07 +0100

    feat():Creo las clases relacionadas con el cliente

---
+++ src/java/entidades/Cliente.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.time.LocalDate;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Bayron
+ */
+@Entity
+@Table(name = "cliente", schema = "fitFlavor")
+@DiscriminatorValue("1")
+@XmlRootElement
+public class Cliente extends Usuario {
+
+    private static final long serialVersionUID = 1L;
+
+    @Enumerated(EnumType.ORDINAL)
+    private EnumSexo sexo;
+
+    private float peso;
+
+    @Enumerated(EnumType.ORDINAL)
+    private EnumObjetivo objetivo;
+
+    private int altura;
+
+    public Cliente(EnumSexo sexo, float peso, EnumObjetivo objetivo, int altura, Integer user_id, String email, String nombreCompleto, LocalDate fechaNacimiento, int telefono, String direccion, int codigoPostal, String contrasenia, EnumPrivilegios privilegio) {
+        super(user_id, email, nombreCompleto, fechaNacimiento, telefono, direccion, codigoPostal, contrasenia, privilegio);
+        this.sexo = sexo;
+        this.peso = peso;
+        this.objetivo = objetivo;
+        this.altura = altura;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public EnumSexo getSexo() {
+        return sexo;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public EnumObjetivo getObjetivo() {
+        return objetivo;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setSexo(EnumSexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public void setObjetivo(EnumObjetivo objetivo) {
+        this.objetivo = objetivo;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (super.getUser_id() != null ? getUser_id().hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((super.getUser_id() == null && other.getUser_id() != null) || (super.getUser_id() != null && !super.getUser_id().equals(other.getUser_id()))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
---
+++ src/java/entidades/Usuario.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.hibernate.annotations.DiscriminatorFormula;
+
+/**
+ *
+ * @author Bayron
+ */
+@NamedQueries({
+    @NamedQuery(
+            name = "signIn", query = "SELECT u FROM User u WHERE login = :loginUsr AND password = :passUsr"
+    ),})
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorFormula("privilegio")
+@DiscriminatorValue("0")
+@Table(name = "usuario", schema = "fitFlavor")
+@XmlRootElement
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer user_id;
+
+    private String email;
+
+    private String nombreCompleto;
+
+    private LocalDate fechaNacimiento;
+
+    private int telefono;
+
+    private String direccion;
+
+    private int codigoPostal;
+
+    private String contrasenia;
+
+    @Enumerated(EnumType.ORDINAL)
+    private EnumPrivilegios privilegio;
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setCodigoPostal(int codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public void setPrivilegio(EnumPrivilegios privilegio) {
+        this.privilegio = privilegio;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public int getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public EnumPrivilegios getPrivilegio() {
+        return privilegio;
+    }
+
+    public Usuario(Integer user_id, String email, String nombreCompleto, LocalDate fechaNacimiento, int telefono, String direccion, int codigoPostal, String contrasenia, EnumPrivilegios privilegio) {
+        this.user_id = user_id;
+        this.email = email;
+        this.nombreCompleto = nombreCompleto;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.codigoPostal = codigoPostal;
+        this.contrasenia = contrasenia;
+        this.privilegio = privilegio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (user_id != null ? user_id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.user_id == null && other.user_id != null) || (this.user_id != null && !this.user_id.equals(other.user_id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
