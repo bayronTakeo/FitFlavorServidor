b79b08091922a80456218f995f6ffda7c06656a0
bayron <bayrontakeoch21@gmail.com>
2023-12-20 11:45:21 +0100

    fix():Reestructuro los paquetes

--- src/java/entidades/Ejercicio.java
+++ src/java/entidades/Ejercicio.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package entities;
+package entidades;
 
+import entities.*;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
