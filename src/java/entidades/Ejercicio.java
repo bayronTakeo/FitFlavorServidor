fca3b208f4095f115be5be74624ecc37a59be28a
gaitza <gaizkagorrotxategi87@gmail.com>
2023-12-21 08:59:55 +0100

    Metodos hashCode(), equals() y toString(), realizados en las clases Dia, Diario y Ejercicio

--- src/java/entidades/Ejercicio.java
+++ src/java/entidades/Ejercicio.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package entities;
+package entidades;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
@@ -15,6 +15,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Esta clase guarda los atributos de Ejercicios
+ *
  * @author gaizka
  */
 @Entity
@@ -50,91 +51,126 @@ public class Ejercicio implements Serializable {
      * Intensidad del Ejercicio
      */
     private String intensidad;
+
     /**
-     * 
+     *
      * @return the id
      */
     public Integer getId() {
         return id;
     }
+
     /**
-     * 
+     *
      * @param id the id to be set
      */
     public void setId(Integer id) {
         this.id = id;
     }
+
     /**
-     * 
+     *
      * @return the nombre
      */
     public String getNombre() {
         return nombre;
     }
+
     /**
-     * 
+     *
      * @param nombre the nombre to be set
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     /**
-     * 
+     *
      * @return the descripcion
      */
     public String getDescripcion() {
         return descripcion;
     }
+
     /**
-     * 
+     *
      * @param descripcion the descripcion to be set
      */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
     /**
-     * 
+     *
      * @return the duracion
      */
     public float getDuracion() {
         return duracion;
     }
+
     /**
-     * 
+     *
      * @param duracion the duracion to be set
      */
     public void setDuracion(float duracion) {
         this.duracion = duracion;
     }
+
     /**
-     * 
+     *
      * @return the KcalQuemadas
      */
     public int getKcalQuemadas() {
         return kcalQuemadas;
     }
+
     /**
-     * 
+     *
      * @param kcalQuemadas the kcalQuemadas to be set
      */
     public void setKcalQuemadas(int kcalQuemadas) {
         this.kcalQuemadas = kcalQuemadas;
     }
+
     /**
-     * 
+     *
      * @return the intensidad
      */
     public String getIntensidad() {
         return intensidad;
     }
+
     /**
-     * 
+     *
      * @param intensidad the intensidad to be set
      */
     public void setIntensidad(String intensidad) {
         this.intensidad = intensidad;
     }
-    
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (super.getClass() != null ? getClass().hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((super.getClass() == null && other.getClass() != null) || (super.getClass() != null && !super.getClass().equals(other.getClass()))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 
 }
