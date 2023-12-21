a6127c43bf597edb8283080fb67fa463a77e141c
Paulaa123123 <paulajimenezbenito@gmail.com>
2023-12-21 14:06:54 +0100

    Excepciones hechas

---
+++ src/java/excepciones/UpdateException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones;
+
+/**
+ *
+ * @author gaizka
+ */
+public class UpdateException extends Exception {
+
+    public UpdateException() {
+
+    }
+
+    public UpdateException(String msg) {
+        super(msg);
+    }
+
+}
